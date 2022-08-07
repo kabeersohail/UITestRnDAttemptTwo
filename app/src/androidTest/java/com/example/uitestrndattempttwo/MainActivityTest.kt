6c9c9370fd9afa13a7669a83b50191e0e2afd302
Sohail <kabeersohel@gmail.com>
2022-08-07 13:45:53 +0530

    can get application variables now in activity test

--- app/src/androidTest/java/com/example/uitestrndattempttwo/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/uitestrndattempttwo/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.example.uitestrndattempttwo
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
@@ -15,7 +16,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-//    private val uiTestApplication: UITestRnDApplication = ApplicationProvider.getApplicationContext()
+    private val uiTestApplication: UITestRnDApplication = ApplicationProvider.getApplicationContext()
 
     @Test
     fun firstTest() = runTest {
@@ -31,7 +32,7 @@ class MainActivityTest {
 
         ActivityScenario.launch(MainActivity::class.java)
 
-        Assert.assertEquals("Yes", "Yes")
+        Assert.assertEquals("Sohail", uiTestApplication.testerName)
     }
 
 }
